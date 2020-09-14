83cecdfedf19c854ae3a72dbd0cc24c072bdb9e1
Bell Wu <wu.bell@berkeley.edu>
2020-09-14 00:49:58 -0700

    basic form with post completed

--- frontend/src/components/ChargeForm.jsx
+++ frontend/src/components/ChargeForm.jsx
@@ -1,29 +1,70 @@
-import React from 'react';
-import { Form, Input, Checkbox, Button } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, DatePicker, Button, Space } from 'antd';
+import moment from 'moment';
+import axios from 'axios';
 
 export default function ChargeForm() {
+  const [ form ] = Form.useForm();
+  const dateFormat = 'MM/DD/YYYY';
+
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post('localhost:5000/purchase/add', values);
+      console.log('response:', response);
+    } catch (e) {
+      console.log(`Axios request failed: ${e}`);
+    }
+  };
+
   return (
-    <Form>
-      <Form.Item
-        label="Item"
-        name="item"
+    <Space>
+      <Form
+      {...layout}
+      form={form}
+      onFinish={onFinish}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Your Name"
+          name="buyer"
+        >
+          <Input />
+        </Form.Item>
 
-      
-      <Form.Item
-        label="Date"
-        name="date"
-      >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Date"
+          name="date"
+        >
+          <DatePicker defaultValue={moment()} format={dateFormat}/>
+        </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">Submit</Button>
-      </Form.Item>
+        <Form.Item
+          label="Item"
+          name="purchaseName"
+        >
+          <Input />
+        </Form.Item>
+        
+        <Form.Item
+          label="Price"
+          name="price"
+        >
+          <Input />
+        </Form.Item>
 
+        <Form.Item>
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </Form.Item>
 
-    </Form>
+      </Form>
+    </Space>
   )
 }

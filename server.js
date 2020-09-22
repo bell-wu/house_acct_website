223e00d68fecb4b0eb86e3066d9310c950181a4b
calebkuo99 <calebkuo@berkeley.edu>
2020-09-21 23:18:26 -0700

    moved contents of backend out to front

--- server.js
+++ server.js
@@ -32,10 +32,10 @@ app.use('/users', usersRouter);
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../frontend/build'));
+  app.use(express.static('/frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '/frontend', 'build', 'index.html'));
   });
 }
 

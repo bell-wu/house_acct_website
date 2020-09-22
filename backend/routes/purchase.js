3940692b1fe3e8fe42d10d1a168d9e61f64011bc
calebkuo99 <calebkuo@berkeley.edu>
2020-09-21 23:05:49 -0700

    first

--- backend/routes/purchase.js
+++ backend/routes/purchase.js
@@ -2,31 +2,7 @@ const router = require('express').Router();
 let Purchase = require('../models/purchase.model');
 const purchaseController = require("./purchaseController");
 
-// router.route('/').get((req, res) => {
-//   Purchase.find()
-//     .then(purchases => res.json(purchases))
-//     .catch(err => res.status(400).json('Error: ' + err));
-// });
-
 router.route('/').get(purchaseController.get);
-
-// router.route('/add').post((req, res) => {
-//   const purchaseName = req.body.purchaseName;
-//   const date = Date.parse(req.body.date);
-//   const price = Number(req.body.price);
-//   const buyer = req.body.buyer;
-//   const consumers = req.body.consumers;
-//   let individualPrice = req.body.price / consumers.length;
-
-  
-
-
-//   const newPurchase = new Purchase({purchaseName, date, price, buyer});
-
-//   newPurchase.save()
-//     .then(() => res.json('Purchase Added!'))
-//     .catch(err => res.status(400).json('Error: ' + err));
-// });
 
 router.route('/add').post(purchaseController.addPost);
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -27,6 +28,16 @@ const usersRouter = require('./routes/users');
 
 app.use('/purchase', purchaseRouter);
 app.use('/users', usersRouter);
+
+// Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

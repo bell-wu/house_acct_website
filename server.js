0b47830bbe7ab04e09e56fcf38dbc017446ec216
calebkuo99 <calebkuo@berkeley.edu>
2020-10-19 01:05:48 -0700

    test backend server * to /

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/users', usersRouter);
 // Set static folder
 app.use(express.static('frontend/build'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 });
 
